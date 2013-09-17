7846211cf4c7b469cd6a1fe3d1a303ac24c49e9d
michellejaeg <michelles82@gmail.com>
2013-09-16 19:18:23 -0700

    some corrections re: probability, styling, etc.

--- 294.7.py
+++ 294.7.py
@@ -2,45 +2,48 @@ from random import randrange
 
 def main ():
     printIntro()
-    n=getInput()
-    wins, losses=simNGames(n)
+    n = getInput()
+    wins, losses = simNGames(n)
     printSummary(n, wins)
 
 def printIntro():
     print ("This program simulates multiple games of craps, a dice game played at casinos.")
 
 def getInput():
-    n=eval(input("\nPlease enter the number of games that you would like to simulate. " ))
+    n=eval(input("\nPlease enter the number of games that you would like to simulate: " ))
     return n
+
+def rollDice():
+    return randrange(1,7) + randrange(1,7)
 
 def simOneGame():
     #Simulates a single craps game
-    initialroll=randrange(1,13)
-    outcome=None
-    if initialroll==7 or initialroll==11:
-        outcome="win"
-    elif initialroll==2 or initialroll==3 or initialroll==12:
-        outcome="lose"
+    initial_roll = rollDice()
+    outcome = None
+    if initial_roll == 7 or initial_roll == 11:
+        outcome = "win"
+    elif initial_roll == 2 or initial_roll == 3 or initial_roll == 12:
+        outcome = "lose"
     else:
-        roll=randrange(1,13)
-        while roll!=7 and roll!=initialroll:
-            roll=randrange(1,13)
-        if roll==initialroll:
-            outcome="win"
-        if roll==7:
-            outcome="lose"
+        roll = rollDice()
+        while roll != 7 and roll != initial_roll:
+            roll = rollDice()
+        if roll == initial_roll:
+            outcome = "win"
+        elif roll == 7:
+            outcome = "lose"
     return outcome
 
 def simNGames(n):
     #Simulates n games of craps
-    wins=0
-    losses=0
+    wins = 0
+    losses = 0
     for i in range(n):
-        outcome=simOneGame()
-        if outcome=="win":
-            wins+=1
+        outcome = simOneGame()
+        if outcome == "win":
+            wins += 1
         else:
-            losses+=1
+            losses += 1
     return wins, losses
 
 def printSummary(n, wins):
